4d10821c1edab9974bbdb6babe81229b6fbcf7a0
Richard Muñoz <richard.munoz@crifa.com>
2020-01-20 17:28:33 -0500

    added TODO

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // TODO implement logic
+
+// "Pico y Placa" predictor.
+// The inputs should be
+// - a license plate number (the full number, not the last digit)
+// - a date (as a String)
+// - a time
+// the program will return whether or not that car can be on the road.
+
+// Note: To develop this application you need to consider the past rules of the Pico&Placa.
+// (Hours: 7:00am - 9:30am / 16:00pm - 19:30).
+// Additional research would be needed to complete the exercise. ​
+
