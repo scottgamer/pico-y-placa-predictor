3b2f89ae6e99eba5adc9424d12792b886a75318b
Richard Munoz <richosojason@msn.com>
2020-01-21 19:38:46 -0500

    added cors middleware

--- index.js
+++ index.js
@@ -13,11 +13,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const port = 4000;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const predictorRoutes = require("./routes/index");
 app.use("/api/pico-placa", predictorRoutes);
